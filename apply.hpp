353f5439392296d15a3f0a3dfd3bf683881a8746
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-02-21 09:37:28 +0100

    remove anonymous namespace in apply

--- apply.hpp
+++ apply.hpp
@@ -3,7 +3,7 @@
 #include <type_traits>
 
 namespace putils {
-    namespace {
+    namespace detail {
         template<typename F, class Tuple, std::size_t ...Is>
         decltype(auto) apply(F && f, Tuple && tuple, std::index_sequence<Is...>) noexcept {
             return f(std::get<Is>(tuple)...);
@@ -12,11 +12,11 @@ namespace putils {
 
     template<typename F, typename ...Args>
     decltype(auto) apply(const std::tuple<Args...> & tuple, F && f) noexcept {
-        return apply(std::forward<F>(f), tuple, std::index_sequence_for<Args...>());
+        return detail::apply(std::forward<F>(f), tuple, std::index_sequence_for<Args...>());
     }
 
     template<typename F, typename ...Args>
     decltype(auto) apply(std::tuple<Args...> & tuple, F && f) noexcept {
-        return apply(std::forward<F>(f), tuple, std::index_sequence_for<Args...>());
+        return detail::apply(std::forward<F>(f), tuple, std::index_sequence_for<Args...>());
     }
 }
