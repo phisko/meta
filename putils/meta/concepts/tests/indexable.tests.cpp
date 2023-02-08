df5afb2166e5fd9c88227439bad6528b19e3ec41
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-08 15:39:25 +0100

    indexable: index type parameter

--- putils/meta/concepts/tests/indexable.tests.cpp
+++ putils/meta/concepts/tests/indexable.tests.cpp
@@ -6,25 +6,37 @@
 
 TEST(indexable, array) {
 	static_assert(putils::indexable<bool[2]>);
+	static_assert(!putils::indexable<bool[2], std::string>);
 	SUCCEED();
 }
 
 TEST(indexable, pointer) {
 	static_assert(putils::indexable<bool *>);
+	static_assert(!putils::indexable<bool *, std::string>);
 	SUCCEED();
 }
 
 TEST(indexable, vector) {
 	static_assert(putils::indexable<std::vector<bool>>);
+	static_assert(!putils::indexable<std::vector<bool>, std::string>);
 	SUCCEED();
 }
 
 TEST(indexable, map) {
 	static_assert(putils::indexable<std::map<int, bool>>);
+	static_assert(!putils::indexable<std::map<int, bool>, std::string>);
+	SUCCEED();
+}
+
+TEST(indexable, string_map) {
+	static_assert(!putils::indexable<std::map<std::string, bool>>);
+	static_assert(putils::indexable<std::map<std::string, bool>, std::string>);
+	static_assert(putils::indexable<std::map<std::string, bool>, const char *>);
 	SUCCEED();
 }
 
 TEST(indexable, false) {
 	static_assert(!putils::indexable<int>);
+	static_assert(!putils::indexable<int, std::string>);
 	SUCCEED();
 }
