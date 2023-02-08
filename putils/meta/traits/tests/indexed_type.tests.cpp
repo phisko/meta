0170d6c04360d32d35c9b2098892bda15f45ba44
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-08 15:39:36 +0100

    indexed_type: index type parameter

--- putils/meta/traits/tests/indexed_type.tests.cpp
+++ putils/meta/traits/tests/indexed_type.tests.cpp
@@ -4,25 +4,31 @@
 // meta
 #include "putils/meta/traits/indexed_type.hpp"
 
-template<typename T>
-using result = std::is_same<float, std::decay_t<putils::indexed_type<T>>>;
+template<typename T, typename Key, typename Expected>
+using result = std::is_same<Expected, std::decay_t<putils::indexed_type<T, Key>>>;
 
 TEST(indexed_type, array) {
-	static_assert(result<float[2]>());
+	static_assert(result<float[2], int, float>());
 	SUCCEED();
 }
 
 TEST(indexed_type, pointer) {
-	static_assert(result<float *>());
+	static_assert(result<float *, int, float>());
 	SUCCEED();
 }
 
 TEST(indexed_type, vector) {
-	static_assert(result<std::vector<float>>());
+	static_assert(result<std::vector<float>, int, float>());
 	SUCCEED();
 }
 
 TEST(indexed_type, map) {
-	static_assert(result<std::map<int, float>>());
+	static_assert(result<std::map<int, float>, int, float>());
+	SUCCEED();
+}
+
+TEST(indexed_type, string_map) {
+	static_assert(result<std::map<std::string, float>, std::string, float>());
+	static_assert(result<std::map<std::string, float>, const char *, float>());
 	SUCCEED();
 }
