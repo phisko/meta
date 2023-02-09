3769fac13491eaf6ba7d8be262520beafa34f2cd
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-02-09 10:43:10 +0100

    clang-format

--- putils/meta/concepts/invocable.hpp
+++ putils/meta/concepts/invocable.hpp
@@ -9,7 +9,8 @@
 
 namespace putils {
 	template<typename T, typename Signature>
-	concept invocable = requires(T && func, putils::function_arguments<Signature> && args) {
-		{ std::apply(std::move(func), std::move(args)) } -> std::convertible_to<putils::function_return_type<Signature>>;
-	};
+	concept invocable =
+		requires(T && func, putils::function_arguments<Signature> && args) {
+			{ std::apply(std::move(func), std::move(args)) } -> std::convertible_to<putils::function_return_type<Signature>>;
+		};
 }
